5e452915fbcd53ccc871166af361f6c2420b2bdd
Secan A. <dariusthegiant@gmail.com>
2023-07-13 23:18:44 +0200

    add wifi and vcard tabs, escape semicolons in ssid/pass

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # qr-generator
 
-small browser tool for making qr codes. text and url for now.
+small browser tool for making qr codes. text, url, wifi credentials, vcard contacts.
+
+## what it does
+
+- four input modes via tabs: text, url, wifi, vcard
+- wifi mode builds the standard `WIFI:T:WPA;S:ssid;P:pass;;` payload that ios and android both recognize
+- vcard mode uses vcard 3.0
 
 ## running it
 
--- app.js
+++ app.js
@@ -18,12 +18,40 @@ tabs.forEach(t => {
   });
 });
 
+// escape commas, semicolons, backslashes for wifi/vcard formats
+function esc(s) {
+  return String(s).replace(/([\\;,":])/g, '\\$1');
+}
+
 function buildPayload() {
   if (activeTab === 'text') {
     return document.getElementById('text-input').value;
   }
   if (activeTab === 'url') {
     return document.getElementById('url-input').value;
+  }
+  if (activeTab === 'wifi') {
+    const ssid = document.getElementById('wifi-ssid').value;
+    const pass = document.getElementById('wifi-pass').value;
+    const enc = document.getElementById('wifi-enc').value;
+    if (!ssid) return '';
+    return `WIFI:T:${enc};S:${esc(ssid)};P:${esc(pass)};;`;
+  }
+  if (activeTab === 'vcard') {
+    const name = document.getElementById('vc-name').value.trim();
+    const phone = document.getElementById('vc-phone').value.trim();
+    const email = document.getElementById('vc-email').value.trim();
+    if (!name && !phone && !email) return '';
+    const lines = [
+      'BEGIN:VCARD',
+      'VERSION:3.0',
+      `FN:${name}`,
+      `N:${name};;;;`,
+    ];
+    if (phone) lines.push(`TEL;TYPE=CELL:${phone}`);
+    if (email) lines.push(`EMAIL:${email}`);
+    lines.push('END:VCARD');
+    return lines.join('\n');
   }
   return '';
 }
@@ -52,8 +80,9 @@ function render() {
   });
 }
 
-document.querySelectorAll('input, textarea').forEach(el => {
+document.querySelectorAll('input, textarea, select').forEach(el => {
   el.addEventListener('input', render);
+  el.addEventListener('change', render);
 });
 
 render();
