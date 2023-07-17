e9cb638f6115c8379dbada73ab6cae9ba3406918
Secan A. <dariusthegiant@gmail.com>
2023-07-17 19:55:32 +0200

    ecl picker, png download, readme polish

--- README.md
+++ README.md
@@ -1,21 +1,38 @@
 # qr-generator
 
-small browser tool for making qr codes. text, url, wifi credentials, vcard contacts.
+small browser tool for making qr codes. supports plain text, urls, wifi credentials, and vcard contacts. renders live to a canvas and lets you save the result as a png.
+
+i needed one for printing wifi cards for an airbnb and got tired of the ad-stuffed sites, so i wrote my own in an evening.
 
 ## what it does
 
 - four input modes via tabs: text, url, wifi, vcard
 - wifi mode builds the standard `WIFI:T:WPA;S:ssid;P:pass;;` payload that ios and android both recognize
 - vcard mode uses vcard 3.0
+- adjustable error correction (L / M / Q / H)
+- download the rendered code as a png
 
 ## running it
 
-no build step.
+no build step, no dependencies to install.
 
 ```
 open index.html
 ```
 
+or serve the folder with anything (`python3 -m http.server`, the live-server extension, etc). qrcode.js loads from a cdn.
+
 ## stack
 
-vanilla html, css, js. qrcode.js 1.5.3 via jsdelivr.
+vanilla html, css, js. [qrcode.js 1.5.3](https://github.com/soldair/node-qrcode) via jsdelivr does the actual encoding.
+
+## files
+
+- `index.html` markup and tabs
+- `style.css` styles
+- `app.js` payload builders and render loop
+- the wifi escape function in `app.js` handles commas and semicolons in ssid or password, which trips up most online generators
+
+## license
+
+source-available under PolyForm Noncommercial 1.0.0. fine for personal use, not for resale or commercial products. see LICENSE.
--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const tabs = document.querySelectorAll('.tab');
 const panels = document.querySelectorAll('.panel');
 const canvas = document.getElementById('qr');
 const status = document.getElementById('status');
+const downloadBtn = document.getElementById('download');
+const eclSelect = document.getElementById('ecl');
 
 let activeTab = 'text';
 
@@ -42,6 +44,7 @@ function buildPayload() {
     const phone = document.getElementById('vc-phone').value.trim();
     const email = document.getElementById('vc-email').value.trim();
     if (!name && !phone && !email) return '';
+    // vcard 3.0, simple
     const lines = [
       'BEGIN:VCARD',
       'VERSION:3.0',
@@ -64,25 +67,39 @@ function render() {
     canvas.height = 280;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     status.textContent = 'type something to generate a code';
+    downloadBtn.disabled = true;
     return;
   }
   QRCode.toCanvas(canvas, payload, {
-    errorCorrectionLevel: 'M',
+    errorCorrectionLevel: eclSelect.value,
     width: 280,
     margin: 2,
     color: { dark: '#1c1c1c', light: '#ffffff' }
   }, err => {
     if (err) {
       status.textContent = 'error: ' + err.message;
+      downloadBtn.disabled = true;
       return;
     }
-    status.textContent = `${payload.length} chars`;
+    status.textContent = `${payload.length} chars, ecl ${eclSelect.value}`;
+    downloadBtn.disabled = false;
   });
 }
 
+// listen on every input
 document.querySelectorAll('input, textarea, select').forEach(el => {
   el.addEventListener('input', render);
   el.addEventListener('change', render);
+});
+
+downloadBtn.addEventListener('click', () => {
+  const url = canvas.toDataURL('image/png');
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = `qr-${activeTab}-${Date.now()}.png`;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
 });
 
 render();
